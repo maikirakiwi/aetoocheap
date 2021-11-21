9efe436c845bd2214ba0ca82cb5e65c4851890b9
Maikiwi <polaris@zerocalorie.us>
2021-11-20 23:21:43 -0800

    Maybe

--- Maikiwi-Doctor-TooCheap-0.2.0/dist/main.js
+++ Maikiwi-Doctor-TooCheap-0.2.0/dist/main.js
@@ -1,18 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-//var fetch = require('node-fetch');
+const https = require('https');
 var fs = require("fs")
 // @ts-ignore
 const resolve_path = internal.path.resolve;
 
-//function fetch_flea_prices() {
-    //fetch('https://raw.githubusercontent.com/maikirakiwi/aetoocheap/main/Maikiwi-Doctor-TooCheap-0.2.0/resources/flea.json').then(response=>{
-        //response.json().then(data=>{
-      //   fs.writeFileSync("../resources/flea.json", JSON.stringify(data))
-    //    })
-  //     })
-//}
-
+function fetch_flea_prices(mod_info) {
+    let folder = `user/mods/${mod_info.author}-${mod_info.name}-${mod_info.version}`;
+    https.get("https://raw.githubusercontent.com/maikirakiwi/aetoocheap/main/Maikiwi-Doctor-TooCheap-0.2.0/resources/flea.json",(res) => {
+    const filePath = fs.createWriteStream(resolve_path(`${folder}/resources/flea.json`));
+    res.pipe(filePath);
+    filePath.on('finish',() => {
+        filePath.close();
+        logger.logSuccess("[MOD] -- TooCheap0.2.0: Synchronized Live Flea Market Prices");
+    })
+    })  
+}
 function update_trader_prices(flea_prices) {
     const db = fileIO.readParsed(resolve_path('user/cache/items.json'));
     const items = db.data;
@@ -34,8 +37,10 @@ function update_flea_prices(flea_prices) {
     fileIO.write(resolve_path('user/cache/templates.json'), db, true);
 }
 exports.mod = (mod_info) => {
-    //  fetch_flea_prices();
-    //logger.logSuccess("[AETooCheap] Synchronized Live Flea Market Prices");
+    if (!serverConfig.rebuildCache) {
+        return;
+    }
+    fetch_flea_prices(mod_info);
     let flea_prices = new Map(Object.entries(require('../resources/flea.json')));
     const ids_blacklist = new Set(require('../resources/blacklist.json'));
     const mod_config = require("../resources/config.json");
@@ -50,11 +55,11 @@ exports.mod = (mod_info) => {
     }));
     if (mod_config.update_trader_prices) {
         update_trader_prices(flea_prices);
-        logger.logSuccess("[AETooCheap] Applied new prices to Traders");
+        logger.logSuccess("[MOD] -- TooCheap0.2.0:  Applied new prices to Traders");
     }
     if (mod_config.update_flea_prices) {
         update_flea_prices(flea_prices);
-        logger.logSuccess("[AETooCheap] Applied new prices to Flea Market");
+        logger.logSuccess("[MOD] -- TooCheap0.2.0:  Applied new prices to Flea Market");
     }
 };
 //# sourceMappingURL=main.js.map
